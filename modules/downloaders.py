c2b92925a14ff6d6c3319580ae2eb8af6bf5c1ef
glooma1 <klonsatklon@gmail.com>
2026-08-23 14:03:49 +0300

    minor fixes

--- modules/downloaders.py
+++ modules/downloaders.py
@@ -48,7 +48,7 @@ def get_short_video(url: str):
             'noplaylist': True,
             'socket_timeout': 30,
             'retries': DOWNLOADS_RETRIES,
-            'extractor_args': {'youtube': {'player_client': ['tv', 'ios', 'web', 'android', 'android_vr'],}},
+            'extractor_args': {'youtube': {'player_client': ['android_vr', 'ios', 'tv_downgraded', 'web_safari']}}
         }
 
     try:
@@ -85,7 +85,7 @@ def get_ytmusic(url: str):
             'noplaylist': True,
             'socket_timeout': 30,
             'retries': DOWNLOADS_RETRIES,
-            'extractor_args': {'youtube': {'player_client': ['web_music', 'tv', 'web', 'android']}},
+            'extractor_args': {'youtube': {'player_client': ['web_music', 'tv_downgraded', 'ios', 'android']}},
             'postprocessors': [
                 {
                     'key': 'FFmpegExtractAudio',
